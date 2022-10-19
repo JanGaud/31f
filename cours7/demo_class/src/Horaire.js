c2fbb190ac95db9394360004d0b89bf8bab4d8fd
JanGaud <JanisGaudreault.jg@gmail.com>
2022-10-19 08:51:21 -0400

    Update Horaire.js

--- cours7/demo_class/src/Horaire.js
+++ cours7/demo_class/src/Horaire.js
@@ -1,5 +1,71 @@
 export default class Horaire{
-    constructor(){
-        console.log("Horaire")
+
+        /**
+         * @type {Cours[]}
+         * @private
+         */
+        #aData;
+        #nbCours;
+        #bActif;
+    
+        /**
+         * Constructeur de la classe, fait la configuration de base du composant
+         * @param {HTMLElement} domParent 
+         * @param {Cours[]} data
+         * @parem {boolean} bRendu - Dessiner ou pas le composant lors de l'instanciation
+         */
+         constructor(domParent, data, bActif){
+            this.domParent = domParent;
+            this.#aData = data;
+            console.log("Horaire");
+            this.#bActif = bActif;
+            if(bActif){
+                this.rendu();   // Appel la méthode d'affichage du composant
+            }
+        }
+        /**
+         * Permet de valider les données du composant
+         * @returns {boolean} Validité du composant
+         * @todo - A faire.
+         */
+        estValide(){
+            let choix = document.querySelectorAll("option:checked");
+            console.log(choix);
+            return (choix.length == this.#nbCours);
+            
+        }
+        setData(donnees){
+            if(donnees.nbCours){
+                this.#nbCours = donnees.nbCours;
+            }
+    
+            console.log(this.#nbCours);
+    
+        }
+        setActif(bActif){
+            this.#bActif = bActif;
+            if(bActif){
+                this.rendu();
+            }
+            else{
+                this.domParent.innerHTML = "";
+            }
+        }
+    
+        /**
+         * Méthode qui affiche le composant dans son parent. 
+         */
+        rendu(){
+            let chaineHTML = `<legend>Inscription</legend>`;
+            chaineHTML += ` <p><label for="nbCours">Cours:</label>
+                                <div class="selectCours">
+                                    <select multiple>`;
+            this.#aData.forEach(function(unCours){
+                chaineHTML += `<option value="${unCours.sigle}">${unCours.nom}</option>`;
+    
+            });
+            chaineHTML +=   `</select></div></p>`;
+    
+            this.domParent.innerHTML = chaineHTML;
+        }
     }
-}
